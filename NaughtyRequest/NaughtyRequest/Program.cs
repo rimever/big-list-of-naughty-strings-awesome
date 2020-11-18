cc5ede712d583268f748b521ee3f39e8bbda42ac
rimever <mot_siva_ordin@hotmail.com>
2020-11-18 21:20:17 +0900

    test code

---
+++ NaughtyRequest/NaughtyRequest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NaughtyRequest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            foreach (var text in File.ReadLines("blns.txt"))
+            {
+                HttpClient httpClient = new HttpClient();
+                string url = $"https://www.google.com/search?q={text}";
+                var httpResponseMessage = await httpClient.GetAsync(url);
+                Console.WriteLine(httpResponseMessage.StatusCode);
+            }
+        }
+    }
+}
